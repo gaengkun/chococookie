45456a17bc6155f77c202212965fb9874653e6e7
seowoochan <gaengkun@gmail.com>
2022-11-10 15:53:26 +0900

    메뉴 뷰 팝업 진행중

--- app/page.jsx
+++ app/page.jsx
@@ -1,47 +1,77 @@
 'use client';
-import React, { useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 import Image from 'next/image';
 import 'swiper/swiper.min.css';
-import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
+import {intro, work, about} from "../pages/constant"
 
-export default function Home() {
+ function MainPage() {
+
+  const [popState, setpopStte] = useState('');
+
+  useEffect( ()=>{
+    console.log('123')
+  },[]
+
+  )
+
   return (
-  <div className='wrap flex-auto p-4'>
+  <div className="wrap h-full">
 
-    <div className='flex justify-between items-center'>
-      <div className='flex w-12 h-12 justify-center items-center text-rose-700 bg-white rounded-full'>D</div>
-      <button>
-        <Image src="/hamburger.svg" width={48} height={48} alt="Picture of the author" />
-      </button>
+    <div className="main_visual h-full relative">
+        <div className="absolute w-full h-full bg-black opacity-30"></div>
+        <div className="h-full space"></div>
+        <div className="h-full mounten"></div>
+        <div className="h-full space"></div>
+        <div className="h-full winter"></div>
     </div>
 
-    <header className='text-xl pt-6 font-semibold'>
-      New <br/>SubJect
-    </header>
+    <div className="flex flex-col items-center justify-center absolute left-0 top-0 w-full h-full text-white">
+        <div className=" w-36 h-36  border-solid block rounded-full bg-white">
+        </div>
+        <div className="main_line_w h-20 bg-white"></div>
+        <div className="border-y py-20">
+        <h2 className="text-center text-6xl font-semibold pb-10 tracking-widest">DIMENSION</h2>
 
-    <div className='pt-12'>
+        <p className="text-center font-thin tracking-widest leading-8">
+        A FULLY RESPONSIVE SITE TEMPLATE DESIGNED BY HTML5 UP AND RELEASED<br/>
+        FOR FREE UNDER THE CREATIVE COMMONS LICENSE.
+        </p>
+        </div>
+        <div className="main_line_w h-20 bg-white"></div>
+        <div className="main_list_btn border rounded">
+          <button href="/intro">INTRO</button>
+          <button href="#">ABOUT</button>
+          <button href="#">WORK</button>
+          <button href="#">CONTACT</button>
+        </div>
+
+    </div>
+
+
+    {/* <div className="pt-12">
       <Swiper
           modules={[Navigation, Pagination, Mousewheel, Keyboard]}
           cssMode={true}
-          navigation={true}
           pagination={true}
-          mousewheel={true}
-          keyboard={true}
-          pagination={
-            {clickable: true}
-          }
+          allowTouchMove={true}
+          simulateTouch={true}
+          // pagination={
+          //   {clickable: true}
+          // }
           className="mySwiper"
         >
-          <SwiperSlide className="bg-white ">Slide 1</SwiperSlide>
-          <SwiperSlide className="bg-white text-black-700">Slide 2</SwiperSlide>
-          <SwiperSlide className="bg-white text-black-700">Slide 3</SwiperSlide>
+          <SwiperSlide className="bg-white text-zinc-800">Slide 1</SwiperSlide>
+          <SwiperSlide className="bg-white text-zinc-800">Slide 2</SwiperSlide>
+          <SwiperSlide className="bg-white text-zinc-800">Slide 3</SwiperSlide>
         </Swiper>
-      </div>
-
-  </div>)
+      </div> */}
+  </div>
+  )
 }
+
+export default MainPage;
---
+++ pages/constant/index.tsx
@@ -0,0 +1,20 @@
+export const intro = {
+    'title' : 'Intro',
+    'imgSrc' : '/images/intro.png',
+    'link' :'intro',
+    'subText' : 'Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my awesome work.',
+}
+
+export const  work= {
+    'title' : 'Work',
+    'imgSrc' : '/images/work.png',
+    'link' :'intro',
+    'subText' : 'Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.',
+}
+
+export const  about= {
+    'title' : 'About',
+    'imgSrc' : '/images/about.png',
+    'about' :'about',
+    'subText' : 'Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.',
+}
